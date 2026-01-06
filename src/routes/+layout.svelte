580bbb929368da64b5c712b3114d6f5fb55ef1bf
atmarx <atm26@drexel.edu>
2026-01-06 14:30:57 -0500

    add logo

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script lang="ts">
 	import '../app.css';
+	import { page } from '$app/stores';
+	import logoSmall from '$lib/assets/x-200.png';
+	import logoLarge from '$lib/assets/x-300.png';
 
 	let { children } = $props();
+
+	const isHome = $derived($page.url.pathname === '/');
+	const isSubpage = $derived(!isHome);
 </script>
 
 <svelte:head>
@@ -10,14 +16,23 @@
 </svelte:head>
 
 <div class="site">
-	<nav>
-		<a href="/" class="site-name">xram.net</a>
-		<div class="nav-links">
-			<a href="/about">About</a>
-			<a href="/projects">Projects</a>
-			<a href="/musings">Musings</a>
-		</div>
-	</nav>
+	<header>
+		<nav>
+			<a href="/" class="site-name" class:has-logo={isSubpage} class:has-logo-large={isHome}>
+				{#if isHome}
+					<img src={logoLarge} alt="xram.net" class="site-logo site-logo-large" />
+				{:else}
+					<img src={logoSmall} alt="xram.net" class="site-logo" />
+				{/if}
+				xram.net
+			</a>
+			<div class="nav-links">
+				<a href="/about">About</a>
+				<a href="/projects">Projects</a>
+				<a href="/musings">Musings</a>
+			</div>
+		</nav>
+	</header>
 
 	<main>
 		{@render children()}
